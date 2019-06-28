0d835be17519042415138812b686a57a381731a7
segur <segur.opus@gmail.com>
2019-06-29 01:04:49 +0900

    Add code to response

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -3,17 +3,17 @@ const functions = require('firebase-functions');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const message = 'GET is sended.';
-  res.json({message});
+  res.json({
+    message: 'GET is sended.',
+    code: req.query.code || '',
+  });
 });
 
 router.post('/', (req, res, next) => {
-  const message = 'POST is sended.';
-  if(req.query.code){
-    res.json({message, code: req.query.code});
-  }else{
-    res.json({message});
-  }
+  res.json({
+    message: 'POST is sended.',
+    code: req.query.code || ''
+  });
 });
 
 module.exports = router;
