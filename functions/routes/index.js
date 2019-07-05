fb3ebc82c7583bea922129e872ecd82cb032cf5a
segur <segur.opus@gmail.com>
2019-07-05 22:34:32 +0900

    Remove post and Add get-oauth

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -15,10 +15,42 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.post('/', (req, res, next) => {
-  res.json({
-    message: 'POST is sended.',
-    code: req.query.code || ''
+/**
+ * For redirect
+ */
+router.get('/oauth', (req, res, next) => {
+  console.info('Start.');
+
+  // parameter varidation
+  if(!req.query.code
+    || !req.query.state
+  ){
+    res.status(400).json({
+      message: 'Error! query not found.',
+      method: req.method,
+      code: req.query.code || '',
+      state: req.query.state || '',
+    });
+  }
+
+  // config varidation
+  if(!functions.config().withings.client_id
+    || !functions.config().withings.client_secret
+    || !functions.config().withings.redirect_uri
+  ){
+    res.status(500).json({
+      message: 'Error! config not found.',
+      method: req.method,
+      code: req.query.code || '',
+      state: req.query.state || '',
+    });
+  }
+  
+  res.status(200).json({
+    message: 'Success!',
+    method: req.method,
+    code: req.query.code || '',
+    state: req.query.state || '',
   });
 });
 
