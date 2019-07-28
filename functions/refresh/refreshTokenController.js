53cdcabc9a4b921b4970456bcf28d2a3a8d1d681
segur <segur.opus@gmail.com>
2019-07-28 23:45:00 +0900

    Add refresh controller

---
+++ functions/refresh/refreshTokenController.js
@@ -0,0 +1,58 @@
+const axios = require('axios');
+const functions = require('firebase-functions');
+const querystring = require('querystring');
+
+module.exports = (req, res, next) => {
+  // config varidation
+  if (!functions.config().withings.client_id
+    || !functions.config().withings.client_secret
+    || !functions.config().withings.redirect_uri
+    || !functions.config().withings.state
+  ) {
+    res.status(500).json({
+      message: 'Error! config not found.',
+      method: req.method,
+      code: req.query.code || '',
+      state: req.query.state || '',
+    });
+  }
+
+  // parameter validation
+  if (!req.body.refresh_token) {
+    res.status(400).json({
+      message: 'Error! refresh_token not found.',
+      method: req.method,
+      code: req.query.code || '',
+    });
+  }
+
+  axios.post(
+    'https://account.withings.com/oauth2/token',
+    querystring.stringify({
+      grant_type: 'refresh_token',
+      client_id: functions.config().withings.client_id,
+      client_secret: functions.config().withings.client_secret,
+      refresh_token: req.body.refresh_token,
+    }),
+  ).then((response) => {
+    console.info('response: ', response.data);
+
+    return res.status(200).json(response.data);
+  }).catch((error) => {
+    if (error.response) {
+      console.error('data: ', error.response.data);
+      console.error('status: ', error.response.status);
+      console.error('headers: ', error.response.headers);
+    } else if (error.request) {
+      console.error('request: ', error.request);
+    } else {
+      console.error('message: ', error.message);
+    }
+    console.error('config: ', error.config);
+
+    return res.status(400).json({
+      message: 'Error!',
+      method: req.method,
+    });
+  });
+};
